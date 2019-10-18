b4e6d0cf31a8b85a9928d2c002ef7bc24ee71825
Michael <44624393+MichaelJapiesBryant@users.noreply.github.com>
2019-10-18 14:15:27 +1300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-/* Setting things up. */
+//Setting the bot up and the required things
 var express = require('express'),
     app = express(),   
     Twit = require('twit'),
-    config = {
-    /* Be sure to update the .env file with your API keys. See how to get them: https://botwiki.org/tutorials/how-to-create-a-twitter-app */      
+    config = {    
       twitter: {
         consumer_key: process.env.CONSUMER_KEY,
         consumer_secret: process.env.CONSUMER_SECRET,
@@ -15,10 +14,10 @@ var express = require('express'),
 
 app.use(express.static('public'));
 
-/* You can use cron-job.org, uptimerobot.com, or a similar site to hit your /BOT_ENDPOINT to wake up your app and make your Twitter bot tweet. */
+//This bot uses cron-job.org. Other free sites are also available. Cron-job is easiest to set up imo
 
 app.all(`/${process.env.BOT_ENDPOINT}`, function(req, res){
-  /* The example below tweets out "Hello world!". */
+
   T.post('statuses/update', { status: countdownMessage() }, function(err, data, response) {
     if (err){
       console.log('error!', err);
@@ -31,10 +30,10 @@ app.all(`/${process.env.BOT_ENDPOINT}`, function(req, res){
 });
 
 var listener = app.listen(process.env.PORT, function(){
-  console.log('Your bot is running on port ' + listener.address().port);
+  console.log('Countdown bot is running on port ' + listener.address().port);
 });
 
-//Set the date to which you want to count down to here!
+//The date here is the 
 var countdownMonth=10;
 var countdownDay=15;
 
@@ -57,37 +56,37 @@ const diffTime = end - now;
 
 var diffDuration = moment.duration(diffTime, 'milliseconds');
 
-/* Returns the message to send as our status update */
+// Returns the message as the status.
 function countdownMessage() {
   
   var status = '';
   
-   if(diffTime > 0) 
+   if(diffTime > 0)  //Start of the  caltulation for the date. Syntax for the message can be changed here. 
 {
 
-     status += 'There is only';
-
-     if(diffDuration.months() > 1 )
+     status += 'There are only';
+    /*                                                  The start of the calculations start here. Because the bot posts an update once every day the minutes and hours is not needed.
+     if(diffDuration.months() > 1 )                     The bot started when there was less than a month remaining, so the months calculation has been commented out. 
      {
-			status += ` ${diffDuration.months()} months`;
+			status += ` ${diffDuration.months()} months,`;
      } 
      else if (diffDuration.months() == 1 )
      {
 			status += ` ${diffDuration.months()} month`;
    	 }
-   	
+   	*/
 	if(diffDuration.days() > 1 )
      {
-			status += ` ${diffDuration.days()} days`;
+			status += ` ${diffDuration.days()} days,`;
      }
      else if (diffDuration.days() == 1 )
      {
 			status += ` ${diffDuration.days()} day`;
    	 }
-    
+    /*
     if(diffDuration.hours() > 1 )
      {
-			status += ` ${diffDuration.hours()} hours`;
+			status += ` ${diffDuration.hours()} hours,`;
      }
      else if (diffDuration.hours() == 1 )
      {
@@ -96,12 +95,13 @@ function countdownMessage() {
 	
 	if(diffDuration.minutes() > 1 )
      {
-			status += ` ${diffDuration.minutes()} minutes`;
+			status += ` and ${diffDuration.minutes()} minutes,`;
      }
      else if (diffDuration.minutes() == 1 )
      {
 			status += ` ${diffDuration.minutes()} minute`;
-   	 }
+      }
+      */
 	
 //Status text until final
 	status += ' left until the release of #StarWarsJediFallenOrder #JediFallenOrder';
